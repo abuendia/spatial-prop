51580217ab746f174be19deea0f62b41005851d6
Alejandro Buendia <abuen@stanford.edu>
2025-11-09 06:49:07 +0000

    Training script for cell type pred

--- bash/run_train.sh
+++ bash/run_train.sh
@@ -1,8 +1,17 @@
 genept_strategy="early_fusion"
-dataset="androvic"
+dataset="reprogramming"
 BASE=/oak/stanford/groups/akundaje/abuen/spatial/spatial-gnn
 # Train with config
-CUDA_VISIBLE_DEVICES=0 python $BASE/src/spatial_gnn/scripts/train_gnn_model_expression.py \
+TRAIN_MULTITASK=False
+if [ "$TRAIN_MULTITASK" = True ]; then
+  TRAIN_MULTITASK_FLAG="--train_multitask"
+else
+  TRAIN_MULTITASK_FLAG=""
+fi
+
+exp_name="celltype_multitask_${TRAIN_MULTITASK_FLAG}"
+
+CUDA_VISIBLE_DEVICES=0 python $BASE/src/spatial_gnn/scripts/train_gnn_with_celltype.py \
       --dataset "$dataset" \
       --base_path "$BASE/data/raw" \
       --k_hop 2 \
@@ -13,9 +22,10 @@ CUDA_VISIBLE_DEVICES=0 python $BASE/src/spatial_gnn/scripts/train_gnn_model_expr
       --learning_rate 0.0001 \
       --loss weightedl1 \
       --epochs 50 \
-      --exp_name "genept_${genept_strategy}" \
+      --exp_name "${exp_name}" \
       --do_eval \
-      --log_to_terminal \
-      --genept_embeddings "/oak/stanford/groups/akundaje/abuen/spatial/spatial-gnn/genept_embeds/zenodo/genept_embed/GenePT_gene_embedding_ada_text.pickle" \
-      --genept_strategy "$genept_strategy" \
+      $TRAIN_MULTITASK_FLAG \
       --predict_celltype
+      # --genept_embeddings "/oak/stanford/groups/akundaje/abuen/spatial/spatial-gnn/genept_embeds/zenodo/genept_embed/GenePT_gene_embedding_ada_text.pickle" \
+      # --genept_strategy "$genept_strategy" \
+
